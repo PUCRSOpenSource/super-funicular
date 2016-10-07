57a13313d8a07847b7f30529036545f34a7d1694
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-10-07 18:41:59 -0300

    Adds report

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,47 +1,47 @@
 touch report
 
-iperf -c $1 -p 5001 -t 30 -w 8k   >> report
+iperf -c $1 -p 5001 -t 30 -w 8k --reportstyle C  >> report
 wait $!
-iperf -c $1 -p 5001 -t 30 -w 16k  >> report
+iperf -c $1 -p 5001 -t 30 -w 16k --reportstyle C  >> report
 wait $!
-iperf -c $1 -p 5001 -t 30 -w 32k  >> report
+iperf -c $1 -p 5001 -t 30 -w 32k --reportstyle C  >> report
 wait $!
-iperf -c $1 -p 5001 -t 30 -w 128k >> report
-wait $!
-
-iperf -c $1 -u -p 5001 -t 30 -w 50k >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 5M  >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 10M >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 50M >> report
+iperf -c $1 -p 5001 -t 30 -w 128k --reportstyle C >> report
 wait $!
 
-iperf -c $1 -u -p 5001 -t 30 -w 50k >> report
+iperf -c $1 -u -p 5001 -t 30 -b 50k --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 5M  >> report
+iperf -c $1 -u -p 5001 -t 30 -b 5M  --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 10M >> report
+iperf -c $1 -u -p 5001 -t 30 -b 10M --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 50M >> report
-wait $!
-
-iperf -c $1 -u -p 5001 -t 30 -w 50k >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 5M  >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 10M >> report
-wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 50M >> report
+iperf -c $1 -u -p 5001 -t 30 -b 50M --reportstyle C >> report
 wait $!
 
-iperf -c $1 -u -p 5001 -t 30 -w 50k >> report
+iperf -c $1 -u -p 5001 -t 30 -b 50k --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 5M  >> report
+iperf -c $1 -u -p 5001 -t 30 -b 5M  --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 10M >> report
+iperf -c $1 -u -p 5001 -t 30 -b 10M --reportstyle C >> report
 wait $!
-iperf -c $1 -u -p 5001 -t 30 -w 50M >> report
+iperf -c $1 -u -p 5001 -t 30 -b 50M --reportstyle C >> report
+wait $!
+
+iperf -c $1 -u -p 5001 -t 30 -b 50k --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 5M  --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 10M --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 50M --reportstyle C >> report
+wait $!
+
+iperf -c $1 -u -p 5001 -t 30 -b 50k --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 5M  --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 10M --reportstyle C >> report
+wait $!
+iperf -c $1 -u -p 5001 -t 30 -b 50M --reportstyle C >> report
 wait $!
 
